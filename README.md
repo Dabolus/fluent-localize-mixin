f4785a912132d24a4d0c56f9d3540950e5f1d598
Giorgio Garasto <mail@dabolus.app>
2018-05-11 21:27:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+localized-lit-element
---
+++ localized-lit-element.js
@@ -0,0 +1,58 @@
+import { LitElement } from '@polymer/lit-element';
+import { MessageContext, ftl } from 'fluent';
+
+/**
+ * A LitElement extension that provides easy l10n out of the box.
+ * @class
+ * @extends LitElement
+ */
+export class LocalizedLitElement extends LitElement {
+  constructor() {
+    super();
+    this.constructor.prototype.__localizationCache = {
+      requests: {},
+      contexts: {},
+    };
+  }
+
+  set locale(locale) {
+    if (locale === this.__locale) {
+      return;
+    }
+
+    if (!this.constructor.prototype.contexts[locale]) {
+      throw new Error(`Resources for "${locale}" were not loaded!`);
+    } else {
+      this.__locale = locale;
+      this.__currentContext = this.constructor.prototype.contexts[locale];
+    }
+  }
+
+  get locale() {
+    return this.__locale;
+  }
+
+  loadResourceForLocale(path, locale) {
+    const cache = this.constructor.prototype.__localizationCache;
+    if (!cache.contexts[locale]) {
+      cache.contexts[locale] = new MessageContext(locale);
+    }
+    if (!cache.requests[path]) {
+      cache.requests[path] = fetch(path)
+        .then((res) => res.text())
+        .then((localeFile) => ftl([localeFile]));
+    }
+    return cache.requests[path]
+      .then((fluentTemplate) => cache.contexts[locale].addMessages(fluentTemplate));
+  }
+
+  localize(key, params) {
+    const cache = this.constructor.prototype.__localizationCache;
+    if (!cache || !this.__locale || !cache.contexts[this.__locale]) {
+      return;
+    }
+    const ctx = cache.contexts[this.__locale];
+    const message = ctx.getMessage(key);
+    return ctx.format(message, params);
+  }
+}
