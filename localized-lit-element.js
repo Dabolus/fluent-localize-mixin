fee33f4344b84054d3eecaa9b2e9949e50b1ad7a
Giorgio Garasto <mail@dabolus.app>
2018-05-12 11:08:43 +0200

    Code refactor
    - Now the cache is shared between all the `LocalizedLitElement`s
    
    - Added jsdoc to each function
    
    - If a value or a locale is missing, the Promise won't reject. The Promises will only reject if fetching a resource fails

--- localized-lit-element.js
+++ localized-lit-element.js
@@ -7,55 +7,63 @@ import { MessageContext, ftl } from 'fluent';
  * @extends LitElement
  */
 export class LocalizedLitElement extends LitElement {
+  /**
+   * The LocalizedLitElement constructor.
+   * Besides calling the super constructor, it also
+   * instantiates the l10n cache.
+   * @constructor
+   */
   constructor() {
     super();
-    this.constructor.prototype.__localizationCache = {
-      requests: {},
-      contexts: {},
-    };
-  }
-
-  set locale(locale) {
-    if (locale === this.__locale) {
-      return;
-    }
-
-    const cache = this.constructor.prototype.__localizationCache;
-    if (!cache || !cache.contexts[locale]) {
-      throw new Error(`Resources for "${locale}" were not loaded!`);
-    } else {
-      this.__locale = locale;
+    if (!LocalizedLitElement.__localizationCache) {
+      LocalizedLitElement.__localizationCache = {};
     }
   }
 
-  get locale() {
-    return this.__locale;
-  }
-
+  /**
+   * Loads the FTL resource at the given path for the given locale.
+   * @param {String} path The path to fetch the FTL resource from
+   * @param {String} locale The locale to associate the fetched resource with
+   * @return {Promise<MessageContext>} A promise that resolves to the MessageContext
+   *         with the fetched messages already added. This promise is mainly used internally,
+   *         but you can also use it yourself to programmatically add other messages to the context
+   */
   loadResourceForLocale(path, locale) {
-    const cache = this.constructor.prototype.__localizationCache;
-    if (!cache.contexts[locale]) {
-      cache.contexts[locale] = new MessageContext(locale);
+    const cache = LocalizedLitElement.__localizationCache;
+    if (!cache[locale]) {
+      cache[locale] = {};
     }
-    if (!cache.requests[path]) {
-      cache.requests[path] = fetch(path)
+    if (!cache[locale][path]) {
+      cache[locale][path] = fetch(path)
         .then((res) => res.text())
-        .then((localeFile) => ftl([localeFile]));
+        .then((localeFile) => ftl([localeFile]))
+        .then((fluentTemplate) => {
+          const ctx = new MessageContext(locale);
+          ctx.addMessages(fluentTemplate);
+          return ctx;
+        });
     }
-    return cache.requests[path]
-      .then((fluentTemplate) => cache.contexts[locale].addMessages(fluentTemplate));
+    this.__contextPromise = cache[locale][path];
+    return this.__contextPromise;
   }
 
+  /**
+   * Localizes a string based on the current language context.
+   * @param {string} key The key to get the localization of.
+   * @param {*} params The params to pass to the localization string.
+   * @return {Promise<string | undefined>} A promise that resolves to the localized string
+   *         at the given key and with the given params (if it exists)
+   */
   localize(key, params) {
-    const cache = this.constructor.prototype.__localizationCache;
-    if (!cache || !this.__locale || !cache.contexts[this.__locale]) {
-      return;
+    if (!this.__contextPromise) {
+      return Promise.resolve();
     }
-    const ctx = cache.contexts[this.__locale];
-    const message = ctx.getMessage(key);
-    if (!message) {
-      return;
-    }
-    return ctx.format(message, params);
+    return this.__contextPromise.then((ctx) => {
+      const message = ctx.getMessage(key);
+      if (!message) {
+        return;
+      }
+      return ctx.format(message, params);
+    });
   }
 }
